83930fca4ac5a53e9f09b1092da613571b96ba9a
naina <nanu02d8@gmail.com>
2020-11-25 12:25:56 +0530

    Add blog Schema

---
+++ server/models/Blogs.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const blogSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+    },
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      max: 50,
+    },
+    content: {
+      type: String,
+      max: 3000,
+    },
+    likes: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+      },
+    ],
+    comments: [
+			{
+				user: {
+					type: mongoose.Schema.ObjectId,
+					ref: 'User'
+				},
+				text: {
+					type: String,
+					required: true
+				}
+			}
+		]
+  },
+  { timestamps: true }
+)
+
+module.exports = mongoose.model('Blog', blogSchema);
+
+
