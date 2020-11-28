b7e3169398becda0cd3d68234469d18f87bc4629
naina <nanu02d8@gmail.com>
2020-11-28 20:38:23 +0530

    Add routes for update and delete blogs

--- server/routes/blog.route.js
+++ server/routes/blog.route.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const { isSignedIn, isAuthenticated } = require("../controllers/auth.controller");
 const { getUserById } = require("../controllers/user.controller");
-const { createBlog, allblogs, upload } = require("../controllers/blog.controller");
+const { createBlog, allblogs, upload, getBlogById, updateBlog, deleteBlog } = require("../controllers/blog.controller");
 const router = express.Router();
 
 // param
 router.param("userId", getUserById);
+router.param("blogId", getBlogById);
 
 // create blog
 router.post("/create/blog/:userId", isSignedIn, isAuthenticated, upload.single("picture"), createBlog);
 
+// updtae blog
+router.put("/update/:userId/:blogId", isSignedIn, isAuthenticated, updateBlog);
+
+// delete blog
+router.delete("/delete/:userId/:blogId",isSignedIn, isAuthenticated, deleteBlog);
+
 // all blogs
 router.get("/blogs", isSignedIn, allblogs);
-
 
 module.exports = router;
