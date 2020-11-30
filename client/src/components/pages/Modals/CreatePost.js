0db5b61d437feea44d2088f39df9b7cb97081fa8
Pranjal Varshney <pranjal25varshney2gmail.com>
2020-11-30 14:22:02 +0530

    image preview added

--- client/src/components/pages/Modals/CreatePost.js
+++ client/src/components/pages/Modals/CreatePost.js
@@ -1,8 +1,10 @@
 import { Grid, TextareaAutosize } from "@material-ui/core"
-import React from "react"
+import React, { useState } from "react"
 import { Modal, Button, Form } from "react-bootstrap"
 
 export const CreatePost = ({ show, handleModal }) => {
+  const [uploadFile, setUploadFile] = useState(null)
+  const [content, setContent] = useState("")
   return (
     <Modal show={show} onHide={handleModal} centered id="input-modal">
       <Modal.Header closeButton>
@@ -10,27 +12,35 @@ export const CreatePost = ({ show, handleModal }) => {
       </Modal.Header>
 
       <Modal.Body>
-        <Grid container justify="space-around" direction="row" md={12}>
+        <Grid container justify="space-between" direction="row" md={12}>
           <Grid item container direction="column" md={4}>
             <Grid item>
               <TextareaAutosize
                 style={{ padding: "10px" }}
                 rowsMin={4}
                 placeholder="Write a caption..."
+                value={content}
+                onChange={(e) => setContent(e.target.value)}
               />
             </Grid>
             <Grid item>
               <Form.File
+                type="file"
+                onChange={(e) =>
+                  setUploadFile(URL.createObjectURL(e.target.files[0]))
+                }
                 id="exampleFormControlFile1"
-                label="Example file input"
+                label="Upload media"
               />
             </Grid>
           </Grid>
-          <Grid item md={7}></Grid>
+          <Grid item md={6}>
+            <img src={uploadFile} alt="input file" width="100%" />
+          </Grid>
         </Grid>
       </Modal.Body>
       <Modal.Footer>
-        <Button size="small" variant="secondary" onClick={handleModal}>
+        <Button variant="secondary" onClick={handleModal}>
           Discard
         </Button>
         <Button size="small" variant="primary" onClick={handleModal}>
