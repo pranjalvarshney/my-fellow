dade9264cdea7723e0b20317d1d9eff55ac92e3a
naina <nanu02d8@gmail.com>
2020-12-13 21:10:26 +0530

    Sent requests completed

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,29 +1,30 @@
-const User = require("../models/User")
-const fs = require("fs")
+const User = require("../models/User");
+const fs = require("fs");
+const async = require("async");
 
 exports.getUserById = (req, res, next, Id) => {
   User.findById(Id).exec((err, user) => {
     if (err) {
       return res.status(400).json({
         errorMsg: "An error occured",
-      })
+      });
     }
     if (!user) {
       return res.status(400).json({
         errorMsg: "User not found",
-      })
+      });
     }
-    req.profile = user
-    next()
-  })
-}
+    req.profile = user;
+    next();
+  });
+};
 
 exports.getUser = (req, res) => {
   //TODO: get back here for password
-  req.profile.encryptedpassword = undefined
-  req.profile.salt = undefined
-  return res.json(req.profile)
-}
+  req.profile.encryptedpassword = undefined;
+  req.profile.salt = undefined;
+  return res.json(req.profile);
+};
 
 exports.updateUser = (req, res) => {
   User.findByIdAndUpdate(
@@ -34,30 +35,30 @@ exports.updateUser = (req, res) => {
       if (err || !user) {
         return res.status(400).json({
           error: "An error occured,  try again later",
-        })
+        });
       }
-      user.encryptedpassword = undefined
-      user.salt = undefined
-      return res.json(user)
+      user.encryptedpassword = undefined;
+      user.salt = undefined;
+      return res.json(user);
     }
-  )
-}
+  );
+};
 
 exports.getAllUsers = (req, res) => {
   User.find().exec((err, users) => {
     if (err) {
       return res.status(400).json({
         errorMsg: "An error occured",
-      })
+      });
     }
     if (!users) {
       return res.status(400).json({
         errorMsg: "User not found",
-      })
+      });
     }
-    return res.json(users)
-  })
-}
+    return res.json(users);
+  });
+};
 exports.getAllBookmarks = (req, res) => {
   User.find({ _id: req.profile._id })
     .populate("bookmark.blog bookmark.post bookmark.ads bookmark.job ")
@@ -65,13 +66,13 @@ exports.getAllBookmarks = (req, res) => {
       if (err) {
         return res.status(400).json({
           errorMsg: "An error occured",
-        })
+        });
       }
-      res.json(bookmarks)
-    })
-}
+      res.json(bookmarks);
+    });
+};
 exports.bookmark = (req, res) => {
-  const { type } = req.body
+  const { type } = req.body;
 
   User.findByIdAndUpdate(
     { _id: req.profile._id, [`${type}`]: type },
@@ -83,10 +84,40 @@ exports.bookmark = (req, res) => {
     if (err) {
       return res
         .status(400)
-        .json({ err, errorMsg: "An error occured, try again later" })
+        .json({ err, errorMsg: "An error occured, try again later" });
     }
-    user.salt = undefined
-    user.encryptedpassword = undefined
-    res.status(200).json(user)
-  })
-}
+    user.salt = undefined;
+    user.encryptedpassword = undefined;
+    res.status(200).json(user);
+  });
+};
+
+exports.addFriend = (req, res) => {
+  const userId = req.body.userId;
+  if (userId == req.profile._id) {
+    userId = null;
+  }
+  User.findById({ _id: userId }).exec((err, friend) => {
+    if (err) {
+      return res
+        .json(400)
+        .json({ errorMsg: "An error occured, try again later" });
+    }
+    User.findByIdAndUpdate(
+      { _id: req.profile._id },
+      {
+        $push: { sentReqs: { userId: friend._id } },
+      },
+      {
+        new: true,
+        useFindAndModify: false,
+      },
+      (err, result) => {
+        if (err) {
+          console.log(err);
+        }
+        res.status(200).json(result);
+      }
+    );
+  });
+};
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -10,16 +10,18 @@ const {
   updateUser,
   bookmark,
   getAllBookmarks,
+  addFriend,
 } = require("../controllers/user.controller")
 const router = express.Router()
 
 router.param("userId", getUserById)
-
-router.get("/user/:userId", isSignedIn, getUser)
+router.get("/user/:userId", isSignedIn, isAuthenticated, getUser)
 router.put("/user/:userId", isSignedIn, isAuthenticated, updateUser)
 router.get("/users", isSignedIn, getAllUsers)
 
 router.put("/bookmark/user/:userId", bookmark)
 router.get("/bookmarks/user/:userId", getAllBookmarks)
+
+router.post("/addFriend/:userId", isSignedIn, isAuthenticated, addFriend)
 
 module.exports = router
