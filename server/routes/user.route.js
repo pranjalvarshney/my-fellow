3927f0cfdda223d2806dd8118f8bbcec0ec664ec
Devesh-19 <devdeveshsingh@gmail.com>
2020-12-14 11:27:00 +0530

    Complete accept friend requests

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,27 +1,29 @@
-const express = require("express")
+const express = require("express");
 const {
-  isSignedIn,
-  isAuthenticated,
-} = require("../controllers/auth.controller")
+	isSignedIn,
+	isAuthenticated,
+} = require("../controllers/auth.controller");
 const {
-  getUserById,
-  getUser,
-  getAllUsers,
-  updateUser,
-  bookmark,
-  getAllBookmarks,
-  addFriend,
-} = require("../controllers/user.controller")
-const router = express.Router()
+	getUserById,
+	getUser,
+	getAllUsers,
+	updateUser,
+	bookmark,
+	getAllBookmarks,
+	addFriend,
+	acceptReq,
+} = require("../controllers/user.controller");
+const router = express.Router();
 
-router.param("userId", getUserById)
-router.get("/user/:userId", isSignedIn, getUser)
-router.put("/user/:userId", isSignedIn, isAuthenticated, updateUser)
-router.get("/users", isSignedIn, getAllUsers)
+router.param("userId", getUserById);
+router.get("/user/:userId", isSignedIn, getUser);
+router.put("/user/:userId", isSignedIn, isAuthenticated, updateUser);
+router.get("/users", isSignedIn, getAllUsers);
 
-router.put("/bookmark/user/:userId", bookmark)
-router.get("/bookmarks/user/:userId", getAllBookmarks)
+router.put("/bookmark/user/:userId", bookmark);
+router.get("/bookmarks/user/:userId", getAllBookmarks);
 
-router.put("/addFriend/:userId", isSignedIn, isAuthenticated, addFriend)
+router.put("/addFriend/:userId", isSignedIn, isAuthenticated, addFriend);
+router.put("/acceptRequest/:userId", isSignedIn, isAuthenticated, acceptReq);
 
-module.exports = router
+module.exports = router;
