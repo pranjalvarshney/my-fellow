bf1e030e8662d3f43021c3d384b934b14bc2008b
Devesh-19 <devdeveshsingh@gmail.com>
2020-12-14 12:29:37 +0530

    Complete unfriend

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -13,6 +13,7 @@ const {
 	addFriend,
 	acceptReq,
 	rejectReq,
+	unfriend,
 } = require("../controllers/user.controller");
 const router = express.Router();
 
@@ -21,11 +22,14 @@ router.get("/user/:userId", isSignedIn, getUser);
 router.put("/user/:userId", isSignedIn, isAuthenticated, updateUser);
 router.get("/users", isSignedIn, getAllUsers);
 
+// Bookmarks
 router.put("/bookmark/user/:userId", bookmark);
 router.get("/bookmarks/user/:userId", getAllBookmarks);
 
+// Friends
 router.put("/addFriend/:userId", isSignedIn, isAuthenticated, addFriend);
 router.put("/acceptRequest/:userId", isSignedIn, isAuthenticated, acceptReq);
 router.put("/rejectRequest/:userId", isSignedIn, isAuthenticated, rejectReq);
+router.put("/unfriend/:userId", isSignedIn, isAuthenticated, unfriend);
 
 module.exports = router;
