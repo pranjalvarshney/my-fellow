d732f5768777cf162552a066d3202d63a3615d2f
Pranjal Varshney <pranjal25varshney@gmail.com>
2020-12-19 02:03:02 +0530

    post functionality added

--- server/controllers/feedback.controller.js
+++ server/controllers/feedback.controller.js
@@ -1,25 +1,25 @@
-const Feedback = require("../models/Feedback");
-const multer = require("multer");
-const path = require("path");
-const fs = require("fs");
-const { getUserById } = require("../controllers/user.controller");
+const Feedback = require("../models/Feedback")
+const multer = require("multer")
+const path = require("path")
+const fs = require("fs")
+const { getUserById } = require("../controllers/user.controller")
 
 exports.getFeedbackById = (req, res, next, Id) => {
   Feedback.findById(Id).exec((err, feedback) => {
     if (err) {
       return res.status(400).json({
         errorMsg: "An error occured",
-      });
+      })
     }
     if (!feedback) {
       return res.status(400).json({
         errorMsg: "Feedback not found",
-      });
+      })
     }
-    req.feedback = feedback;
-    next();
-  });
-};
+    req.feedback = feedback
+    next()
+  })
+}
 
 fs.mkdir("uploads", (err) => {
   if (err) {
@@ -27,12 +27,12 @@ fs.mkdir("uploads", (err) => {
   fs.mkdir("uploads/feedbacks", (err) => {
     if (err) {
     }
-  });
-});
+  })
+})
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/feedbacks");
+    cb(null, "uploads/feedbacks")
   },
   filename: (req, file, cb) => {
     cb(
@@ -43,9 +43,9 @@ const storage = multer.diskStorage({
           .replace(/-|:|\/|\.|,|/g, "")
           .replace(/ /g, "_") +
         path.extname(file.originalname)
-    );
+    )
   },
-});
+})
 const fileFilter = (req, file, cb) => {
   if (
     file.mimetype == "image/jpeg" ||
@@ -54,30 +54,31 @@ const fileFilter = (req, file, cb) => {
     file.mimetype == "image/svg+xml" ||
     file.mimetype == "video/mp4"
   ) {
-    cb(null, true);
+    cb(null, true)
   } else {
-    cb(null, false);
+    cb(null, false)
   }
-};
-exports.upload = multer({ storage: storage, fileFilter: fileFilter });
+}
+exports.upload = multer({ storage: storage, fileFilter: fileFilter })
 
 // create feedback
 exports.createFeedback = (req, res) => {
-  const { user, feedback } = req.body;
-  var picture;
+  const user = req.profile
+  const { feedback } = req.body
+  var picture
   if (req.file) {
-    picture = req.file.path;
+    picture = req.file.path
   }
-  const newFeedback = Feedback({ user, feedback, picture });
+  const newFeedback = Feedback({ user, feedback, picture })
   newFeedback.save((err, feedback) => {
     if (err) {
-      res.status(400).json({
+      return res.status(400).json({
         errorMsg: "An error occured",
-      });
+      })
     }
-    return res.status(200).json(feedback);
-  });
-};
+    res.status(200).json(feedback)
+  })
+}
 
 //get all feedbacks
 exports.allFeedbacks = (req, res) => {
@@ -85,12 +86,12 @@ exports.allFeedbacks = (req, res) => {
     if (err) {
       res.status(400).json({
         errorMsg: "An error occured",
-      });
+      })
     }
 
-    return res.json(feedbacks);
-  });
-};
+    return res.json(feedbacks)
+  })
+}
 
 //Read a particular feedback
 exports.getFeedback = (req, res) => {
@@ -98,8 +99,8 @@ exports.getFeedback = (req, res) => {
     if (err) {
       res.status(400).json({
         errorMsg: "An error occured",
-      });
+      })
     }
-    return res.json(feedback);
-  });
-};
+    return res.json(feedback)
+  })
+}
